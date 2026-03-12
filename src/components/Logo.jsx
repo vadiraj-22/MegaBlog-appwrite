5afc28d6211dcd7bcde684f9128cef61d1ecd26e
vadiraj-22 <vadirajjoshi22504@gmail.com>
2026-03-12 09:47:13 +0530

    fix: Logo Display

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <div className=' flex flex text-xl text-bold items-center gap-4'>
-      <img className='w-12'  src="./public/logo.png" alt="" />
+      <img className='w-12'  src="/Logo.png" alt="Logo" />
     </div>
   )
 }
