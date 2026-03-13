01603b83b6927e25d2779efd454df1b2307a31fb
vadiraj-22 <vadirajjoshi22504@gmail.com>
2026-03-13 22:36:00 +0530

    Favicon update

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue = ""}) {
             control={control}
             render={({field:{onChange}})=>(
                 <Editor
-                apiKey={import.meta.env.VITE_TINYMCE_API_KEY || ""}
+                apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
                 initialValue={defaultValue}
                 init={{
             initialValue: defaultValue,
